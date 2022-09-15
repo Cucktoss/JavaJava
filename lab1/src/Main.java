600aa6782e67c818cbb0e7a7e529014e1aecbbe2
kante <Kantemirov2003@mail.ru>
2022-09-15 03:39:35 +0300

    s

---
+++ lab1/src/Main.java
@@ -0,0 +1,7 @@
+import classes.*;
+public class Main {
+    public static void main(String[] args){
+        HarmonyRow x = new HarmonyRow();
+        x.showRow10();
+    }
+}
---
+++ lab1/src/classes/HarmonyRow.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class HarmonyRow {
+    public void showRow10(){
+        for(int i = 1; i<=10;i++){
+            System.out.print(1./i+" ");
+        }
+    }
+}
