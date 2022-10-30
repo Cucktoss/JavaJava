adee414addae67a17b632f6a0c491b43dd59fe72
kante <Kantemirov2003@mail.ru>
2022-10-30 21:52:03 +0300

    b

--- pr5/src/Main.java
+++ pr5/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         RecOdd();
         ShowNum1(12345678);
+        System.out.println();
         ShowNum2(12345678);
     }
 }
